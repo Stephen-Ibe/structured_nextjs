5965d89aaaa4eb2ede196e6eec89660624c6a070
Kalson <C_ibeste@gcn.ab-inbev.com>
2025-05-13 06:44:03 +0100

    create post btn

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -13,7 +13,9 @@ const Posts = () => {
       <div className="border p-4 flex flex-col gap-y-2">
         <div className="flex items-center justify-between px-4">
           <h1>All Posts</h1>
-          <button className="border-2 py-2.5 px-4">Create New Post</button>
+          <button className="border-2 py-2.5 px-4 cursor-pointer bg-white text-black hover:bg-gray-300 hover:text-black">
+            Create New Post
+          </button>
         </div>
         <hr />
         {allPosts?.map(({ id, title }: TGetPosts) => (
